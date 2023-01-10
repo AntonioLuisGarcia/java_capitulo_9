31206779e80297eb9b3dc5d5f4f6997342b474e0
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-01-10 12:15:29 +0100

    Clase Vehiculo

---
+++ 9.2_POO_Java/Ejercicio2/Vehiculo.java
@@ -0,0 +1,28 @@
+public abstract class Vehiculo{
+
+    private static int kilometrosTotales;
+    private static int vehiculosCreados;
+    private int kilometrosRecorridos;
+
+    Vehiculo(int kilometrosRecorridos, int vehiculosCreados){
+        this.kilometrosRecorridos = kilometrosRecorridos;
+        vehiculosCreados++;
+    }
+
+    public int getKilometrosRecorridos(){
+        return kilometrosRecorridos;
+    }
+
+    public static int getKilometrosTotales(){
+        return kilometrosTotales;
+    }
+
+    public void recorre(int distancia){
+        Vehiculo.kilometrosTotales += distancia;
+        this.kilometrosRecorridos += distancia;
+    }
+
+    public static int getVehiculosCreados() {
+        return vehiculosCreados;
+    }
+}

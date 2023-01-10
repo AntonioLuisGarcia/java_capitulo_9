164e487c54d1c3f81cdacaad9abce23a3c723840
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-01-10 12:12:44 +0100

    clase caballo

---
+++ 9.2_POO_Java/Ejercicio1/Caballo.java
@@ -0,0 +1,63 @@
+class Caballo{
+
+    private String nombre;
+    private String color;
+    private int edad;
+    private int carrerasGanadas;
+
+    Caballo(String nombre, String color, int edad, int carrerasGanadas){
+        this.nombre = nombre;
+        this.color = color;
+        this.edad = edad;
+        this.carrerasGanadas = carrerasGanadas;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+
+    public int getEdad(){
+        return edad;
+    }
+
+
+    public void setCarrerasGanadas(int carrerasGanadas){
+        this.carrerasGanadas = carrerasGanadas;
+    }
+
+    public int getCarrerasGanadas(){
+        return carrerasGanadas;
+    }
+
+
+    public void cabalga(){
+        System.out.println("Tocotoc tocotoc tocotoc");
+    }
+
+    public void relincha(){
+        System.out.println("Hiiiiiiieeeeee");
+    }
+
+    public void rumia(){
+        System.out.println("Ñam ñam ñam");
+    }
+
+}

109d0270c40ec6e838b538603dc245ab0be7d205
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-01-10 12:16:04 +0100

    Clase Coche

---
+++ 9.2_POO_Java/Ejercicio2/Coche.java
@@ -0,0 +1,17 @@
+class Coche extends Vehiculo{
+
+    private int caballos;
+
+    public Coche(int kilometrosRecorridos, int caballos) {
+        super(kilometrosRecorridos, caballos);
+        this.caballos = caballos;
+    }
+
+    public int getCaballos(){
+        return caballos;
+    }
+
+    public void quemaRueda(){
+        System.out.println("Ppphhhhhhhhhsssss");
+    }
+}

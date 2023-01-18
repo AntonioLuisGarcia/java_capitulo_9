b9167234f60040cc93715c93916c0501a121b7d9
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-01-18 22:27:51 +0100

    main espaciado

--- practicasExamenes/arraysConObjetos_Baraja/Principal.java
+++ practicasExamenes/arraysConObjetos_Baraja/Principal.java
@@ -6,8 +6,10 @@ public class Principal {
 
 	public static void main (String args[]) {
 		Baraja baraja = new Baraja();
+
 		baraja.pasaUnaCartaDeAbajoHastaArriba();
 		baraja.pasaUnaCartaDeArribaHastaAbajo();
+		
 		baraja.mezcla();
 		baraja.ordena();
 		
